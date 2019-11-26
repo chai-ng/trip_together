e9174d37543629cb10b8bbed1033f534454029c4
Chai Ng <chaishean@gmail.com>
2019-11-27 01:12:02 +1100

    got access token and Google Calendar API scope working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    require 'google/apis/calendar_v3'
+    require 'date'
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     # provider :developer unless Rails.env.production?
-    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true, scope: "openid profile email https://www.googleapis.com/auth/calendar"
 end
