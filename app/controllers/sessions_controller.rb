f3825a5c2cd52fe52ffa3aae545716c47b56036f
Chai Ng <chaishean@gmail.com>
2019-11-26 15:40:57 +1100

    start of creating databases

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,17 @@ class SessionsController < ApplicationController
     # GET /auth/google_oauth2/callback
     def create
       user_info = request.env["omniauth.auth"]
-  
+
+      # if user exists, then return user from PSQL
+      
+      # If user does not exist in Users table, then create new user
       user           = User.new
       user.uid        = user_info["uid"]
       user.first_name      = user_info["info"]["first_name"]
       user.last_name      = user_info["info"]["last_name"]
       user.image_url = user_info["info"]["image"]
       user.email = user_info["info"]["email"]
+      
       user.save
   
     #   session[:user] = Marshal.dump user
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   get "/login", to: redirect("/auth/google_oauth2")
+  get "/auth/google_oauth2/callback", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
 
-  get "/auth/google_oauth2/callback", to: "sessions#create"
 
 end
