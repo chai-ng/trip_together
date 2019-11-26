3930322a44c93c648da94f81f97f702707a80e30
Chai Ng <chaishean@gmail.com>
2019-11-26 16:24:46 +1100

    setup controllers

--- app/controllers/travellers_controller.rb
+++ app/controllers/travellers_controller.rb
@@ -1,2 +1,36 @@
 class TravellersController < ApplicationController
+    def index
+    # Create new resource in the database
+    # GET /resource/new
+    end
+
+    def new
+    # Create new resource in the database
+    # POST /resource/new
+    end
+
+    def create
+    # Creating a new resource in the database
+    # POST /resource
+    end
+
+    def show
+    # Show specific trip from /trips/:id
+    # GET /resource/:id
+    end
+
+    def edit
+    # Show HTML form to edit a specific resource
+    # GET /resource/:id/edit
+    end
+
+    def update
+    # Update a specific resource in the database
+    # GET /resource/:id
+    end
+
+    def delete
+    # Destory a specific resource in the database from /trips/:id
+    # DELETE /resource/:id
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :session, only: [:create, :destroy]
 
+  # homepage
   root 'welcome#index'
+  
+  # sessions
   get "/login", to: redirect("/auth/google_oauth2")
   get "/auth/google_oauth2/callback", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
-
 
 end
