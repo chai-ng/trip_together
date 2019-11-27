4c4f00d1f44d1b88f4df55ccdbf07d6008451f12
Chai Ng <chaishean@gmail.com>
2019-11-27 15:52:50 +1100

    start of voting API

---
+++ app/controllers/api/votes_controller.rb
@@ -0,0 +1,38 @@
+class Api::TripsController < ApplicationController
+    def index
+    # List all resources in the database
+    # GET /resource
+        render json: Vote.where(trip_id: params[:trip_id])
+    end
+
+    def create
+        vote = Vote.new
+        vote.place_id = params[:place_id]
+        vote.trip_id = params[:trip_id]
+        vote.type = params[:type]
+        vote.user_id = current_user.id
+        vote.save
+    end
+
+    def show
+    # Show specific trip from /trips/:id
+    # GET /resource/:id
+        render json: Vote.find_by(id: params[:id])
+    end
+
+    def edit
+    # Show HTML form to edit a specific resource
+    # GET /resource/:id/edit
+    end
+
+    def update
+    # Update a specific resource in the database
+    # GET /resource/:id
+    end
+
+    def delete
+    # Destory a specific resource in the database from /trips/:id
+    # DELETE /resource/:id
+        Vote.delete_by(id: params[:id])
+    end
+end
---
+++ db/migrate/20191127044306_change_column_name.rb
@@ -0,0 +1,9 @@
+class ChangeColumnName < ActiveRecord::Migration[6.0]
+  def up
+
+  end
+
+  def down
+
+  end
+end
