0c1e353873fb2788e2f9f33b55e91e7cedba4ec1
nikkiricks <nikki.ricks@gmail.com>
2019-11-27 22:25:28 +1100

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,40 +7,66 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Place.create!([
-  { "name": "Buckingham Palace", "latitude": "51.501564","longitude": "-0.141944"},
-  { "name": "Westminster Abbey", "latitude": "51.499581", "longitude": "-0.127309"},
-  { "name": "Big Ben", "latitude": "51.500792", "longitude": "-0.124613"}
-  ])
+# Place.create!([
+#   {
+#     "place_id":
+#     "trip_id":
+#     "name": "Buckingham Palace"
+#     "international_phone_number":
+#     "formatted_address":
+#     "latitude": "51.501564"
+#     "longitude": "-0.141944"
+#     "website":
+#     "opening_hours":
+#     "type":
+#     "rating":
+#     "price_level":
+#     "cost":
+#     "user_id":
+#     },
+#   {
+#     "place_id":
+#     "trip_id":
+#     "name": "Westminster Abbey"
+#     "international_phone_number":
+#     "formatted_address":
+#     "latitude": "51.499581"
+#     "longitude": "-0.127309"
+#     "website":
+#     "opening_hours":
+#     "type":
+#     "rating":
+#     "price_level":
+#     "cost":
+#     "user_id":
+#     },
+#   {
+#     "place_id":
+#     "trip_id":
+#     "name": "Big Ben"
+#     "international_phone_number":
+#     "formatted_address":
+#     "latitude": "51.500792"
+#     "longitude": "-0.124613"
+#     "website":
+#     "opening_hours":
+#     "type":
+#     "rating":
+#     "price_level":
+#     "cost":
+#     "user_id":
+#       }
+# ])
 
-  Place.create!([
-    {
-      "place_id":
-      "trip_id":
-      "name":
-      "international_phone_number":
-      "formatted_address":
-      "latitude":
-      "longitude":
-      "website":
-      "opening_hours":
-      "type":
-      "rating":
-      "price_level":
-      "cost":
-      "user_id":
-        }
-  ])
-
-  Trip.create!([
-    {
-      "name":
-      "location":
-      "start_date":
-      "end_date":
-      "created_by":
-      "calendar_name":
-      "calendar_timezone":
-      "calendar_id":
-    }
-  ])
+# Trip.create!([
+#   {
+#     "name":
+#     "location":
+#     "start_date":
+#     "end_date":
+#     "created_by":
+#     "calendar_name":
+#     "calendar_timezone":
+#     "calendar_id":
+#   }
+# ])
