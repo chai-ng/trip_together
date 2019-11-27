da65b9236c6818e9006bf9e4d6463d2b40d1e473
Chai Ng <chaishean@gmail.com>
2019-11-27 21:50:41 +1100

    implemented google-api-client

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-    helper_method :logged_in?, :current_user
+    require 'googleauth'
+    require 'googleauth/stores/redis_token_store'
+    require 'google/apis/calendar_v3'
+
+    helper_method :logged_in?, :current_user, :credentials_for
     
     def logged_in?
         session.has_key? :user
@@ -8,5 +12,22 @@ class ApplicationController < ActionController::Base
     def current_user
         # Marshal.load session[:user] if logged_in?
         User.find_by(id: session[:user]) if logged_in?
+    end
+
+    def credentials_for(scope)
+        client_id = Google::Auth::ClientId.new(ENV['GOOGLE_CLIENT_ID'],ENV['GOOGLE_CLIENT_SECRET'])
+        token_store = Google::Auth::Stores::RedisTokenStore.new(redis: Redis.new)
+        authorizer = Google::Auth::WebUserAuthorizer.new(client_id, scope, token_store)
+        user_id = current_user.uid
+        redirect_to "/" if user_id.nil?
+        credentials = authorizer.get_credentials(user_id, request)
+        if credentials.nil?
+          redirect authorizer.get_authorization_url(login_hint: user_id, request: request)
+        end
+        credentials
+    end
+
+    def resize(url, width)
+        url.sub(/s220/, sprintf('s%d', width))
     end
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -66,4 +66,16 @@ class TripsController < ApplicationController
 
         render :trip_calendar
     end
+
+    def authcalendar
+        calendar = Google::Apis::CalendarV3::CalendarService.new
+        calendar.authorization = credentials_for(Google::Apis::CalendarV3::AUTH_CALENDAR)
+        calendar_id = Trip.find_by(id: params[:id]).calendar_id
+        @result = calendar.list_events(calendar_id,
+                                        max_results: 10,
+                                        single_events: true,
+                                        order_by: 'startTime',
+                                        time_min: Time.now.iso8601)
+        render :auth_calendar
+    end
 end
