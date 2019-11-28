76ece539494519b7a00b1a544d7f550696b3557c
Chai Ng <chaishean@gmail.com>
2019-11-29 02:04:05 +1100

    added seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,72 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
-# Place.create!([
-#   {
-#     "place_id":
-#     "trip_id":
-#     "name": "Buckingham Palace"
-#     "international_phone_number":
-#     "formatted_address":
-#     "latitude": "51.501564"
-#     "longitude": "-0.141944"
-#     "website":
-#     "opening_hours":
-#     "type":
-#     "rating":
-#     "price_level":
-#     "cost":
-#     "user_id":
-#     },
-#   {
-#     "place_id":
-#     "trip_id":
-#     "name": "Westminster Abbey"
-#     "international_phone_number":
-#     "formatted_address":
-#     "latitude": "51.499581"
-#     "longitude": "-0.127309"
-#     "website":
-#     "opening_hours":
-#     "type":
-#     "rating":
-#     "price_level":
-#     "cost":
-#     "user_id":
-#     },
-#   {
-#     "place_id":
-#     "trip_id":
-#     "name": "Big Ben"
-#     "international_phone_number":
-#     "formatted_address":
-#     "latitude": "51.500792"
-#     "longitude": "-0.124613"
-#     "website":
-#     "opening_hours":
-#     "type":
-#     "rating":
-#     "price_level":
-#     "cost":
-#     "user_id":
-#       }
-# ])
-
-# Trip.create!([
-#   {
-#     "name":
-#     "location":
-#     "start_date":
-#     "end_date":
-#     "created_by":
-#     "calendar_name":
-#     "calendar_timezone":
-#     "calendar_id":
-#   }
-# ])
+User.create!([
+  {uid: "105018549157264484999", first_name: "Chai Shean", last_name: "Ng", image_url: "https://lh3.googleusercontent.com/a-/AAuE7mCLoV9UyiVdikNIPInYZsgM8WKyRsMsTAYcnCSHOg", email: "chaishean@gmail.com"}
+])
+Trip.create!([
+  {name: "test trip 2", location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
+  {name: "test trip 2", location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
+  {name: "test trip 2", location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
+  {name: nil, location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
+  {name: nil, location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
+  {name: "redirect me", location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
+  {name: nil, location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
+  {name: nil, location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
+  {name: "Test Trip", location: "Melbourne", start_date: "2019-12-01 00:00:00", end_date: "2019-12-31 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
+  {name: "test trip", location: nil, start_date: nil, end_date: nil, calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: "r8dr8gc90983q36668u6t4q9m8@group.calendar.google.com"},
+  {name: "Paris Trip", location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
+  {name: "Paris Trip", location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil}
+])
+Place.create!([
+  {place_id: nil, trip_id: 1, name: "The Ian Potter Foundation", international_phone_number: nil, formatted_address: "Level 3/111 Collins St, Melbourne VIC 3000, Australia", latitude: "144.9702436", longitude: "-37.8145949", website: nil, opening_hours: nil, rating: 4.5, price_level: nil, cost: nil, user_id: 5, establishment_type: ["point_of_interest", "establishment"], upvote: 0, downvote: 0},
+  {place_id: nil, trip_id: 1, name: "The General Assembly", international_phone_number: nil, formatted_address: "29 S Wharf Promenade, South Wharf VIC 3006, Australia", latitude: "144.9507139", longitude: "-37.824143", website: nil, opening_hours: nil, rating: 4.2, price_level: nil, cost: nil, user_id: 5, establishment_type: ["restaurant", "bar", "food", "point_of_interest", "establishment"], upvote: 0, downvote: 0},
+  {place_id: nil, trip_id: 1, name: "Rockpool Bar & Grill", international_phone_number: nil, formatted_address: "Crown Entertainment Complex, 8 Whiteman St, Southbank VIC 3006, Australia", latitude: "144.96027", longitude: "-37.8218482", website: nil, opening_hours: nil, rating: 4.2, price_level: nil, cost: nil, user_id: 5, establishment_type: ["bar", "restaurant", "food", "point_of_interest", "establishment"], upvote: 1, downvote: 0},
+  {place_id: nil, trip_id: 1, name: "National Gallery of Victoria", international_phone_number: nil, formatted_address: "180 St Kilda Rd, Melbourne VIC 3006, Australia", latitude: "144.9689278", longitude: "-37.8225942", website: nil, opening_hours: nil, rating: 4.7, price_level: nil, cost: nil, user_id: 5, establishment_type: ["art_gallery", "tourist_attraction", "point_of_interest", "establishment"], upvote: 1, downvote: 0},
+  {place_id: nil, trip_id: 1, name: "State Library Victoria", international_phone_number: nil, formatted_address: "328 Swanston St, Melbourne VIC 3000, Australia", latitude: "144.9651897", longitude: "-37.8098087", website: nil, opening_hours: nil, rating: 4.7, price_level: nil, cost: nil, user_id: 5, establishment_type: ["art_gallery", "library", "tourist_attraction", "point_of_interest", "establishment"], upvote: 1, downvote: 0}
+])
+Vote.create!([
+  {place_id: 5, trip_id: 1, user_id: 5, vote_type: "upvote"},
+  {place_id: 11, trip_id: 1, user_id: 5, vote_type: "upvote"},
+  {place_id: 7, trip_id: 1, user_id: 5, vote_type: "upvote"}
+])
+Traveller.create!([
+  {trip_id: 11, user_email: "chaishean@gmail.com", existing_user: true, accepted_invite: true, user_budget: nil},
+  {trip_id: 12, user_email: "chaishean@gmail.com", existing_user: true, accepted_invite: true, user_budget: nil},
+  {trip_id: 11, user_email: "emile@gmail.com", existing_user: false, accepted_invite: true, user_budget: nil}
+])
