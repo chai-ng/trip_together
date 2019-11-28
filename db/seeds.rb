7f6793afd510ff2ba0f4c7397c2c2ccfa2fb6773
Chai Ng <chaishean@gmail.com>
2019-11-29 10:06:34 +1100

    updated seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,8 @@ User.create!([
   {uid: "105018549157264484999", first_name: "Chai Shean", last_name: "Ng", image_url: "https://lh3.googleusercontent.com/a-/AAuE7mCLoV9UyiVdikNIPInYZsgM8WKyRsMsTAYcnCSHOg", email: "chaishean@gmail.com"}
 ])
 Trip.create!([
-  {name: "test trip 2", location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
-  {name: "test trip 2", location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
-  {name: "test trip 2", location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
-  {name: nil, location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
-  {name: nil, location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
-  {name: "redirect me", location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
-  {name: nil, location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
-  {name: nil, location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
-  {name: "Test Trip", location: "Melbourne", start_date: "2019-12-01 00:00:00", end_date: "2019-12-31 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
-  {name: "test trip", location: nil, start_date: nil, end_date: nil, calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: "r8dr8gc90983q36668u6t4q9m8@group.calendar.google.com"},
-  {name: "Paris Trip", location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil},
-  {name: "Paris Trip", location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: nil}
+  {name: "Trip 1", location: nil, start_date: "2019-12-01 00:00:00", end_date: "2019-12-31 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 5, calendar_id: "r8dr8gc90983q36668u6t4q9m8@group.calendar.google.com"},
+  {name: "Paris Trip", location: "Paris", start_date: "2019-12-29 00:00:00", end_date: "2020-01-18 00:00:00", calendar_name: nil, calendar_time_zone: nil, user_id: 1, calendar_id: nil}
 ])
 Place.create!([
   {place_id: nil, trip_id: 1, name: "The Ian Potter Foundation", international_phone_number: nil, formatted_address: "Level 3/111 Collins St, Melbourne VIC 3000, Australia", latitude: "144.9702436", longitude: "-37.8145949", website: nil, opening_hours: nil, rating: 4.5, price_level: nil, cost: nil, user_id: 5, establishment_type: ["point_of_interest", "establishment"], upvote: 0, downvote: 0},
@@ -23,12 +13,11 @@ Place.create!([
   {place_id: nil, trip_id: 1, name: "State Library Victoria", international_phone_number: nil, formatted_address: "328 Swanston St, Melbourne VIC 3000, Australia", latitude: "144.9651897", longitude: "-37.8098087", website: nil, opening_hours: nil, rating: 4.7, price_level: nil, cost: nil, user_id: 5, establishment_type: ["art_gallery", "library", "tourist_attraction", "point_of_interest", "establishment"], upvote: 1, downvote: 0}
 ])
 Vote.create!([
-  {place_id: 5, trip_id: 1, user_id: 5, vote_type: "upvote"},
-  {place_id: 11, trip_id: 1, user_id: 5, vote_type: "upvote"},
-  {place_id: 7, trip_id: 1, user_id: 5, vote_type: "upvote"}
+  {place_id: 1, trip_id: 1, user_id: 1, vote_type: "upvote"},
+  {place_id: 1, trip_id: 1, user_id: 1, vote_type: "upvote"},
+  {place_id: 1, trip_id: 1, user_id: 1, vote_type: "downvote"}
 ])
 Traveller.create!([
-  {trip_id: 11, user_email: "chaishean@gmail.com", existing_user: true, accepted_invite: true, user_budget: nil},
-  {trip_id: 12, user_email: "chaishean@gmail.com", existing_user: true, accepted_invite: true, user_budget: nil},
-  {trip_id: 11, user_email: "emile@gmail.com", existing_user: false, accepted_invite: true, user_budget: nil}
+  {trip_id: 1, user_email: "chaishean@gmail.com", existing_user: true, accepted_invite: true, user_budget: nil},
+  {trip_id: 1, user_email: "emile@gmail.com", existing_user: false, accepted_invite: true, user_budget: nil}
 ])
