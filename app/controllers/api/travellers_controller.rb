54d1984d76f08eb995ba24071d5e69df0090c0a6
Emile Desfontaines <emile.desfontaines@gmail.com>
2019-11-28 15:35:45 +1100

    commit to do a fetch

--- app/controllers/api/travellers_controller.rb
+++ app/controllers/api/travellers_controller.rb
@@ -2,7 +2,10 @@ class Api::TravellersController < ApplicationController
     def index
         # return me all users associated with this trip
         connection = ActiveRecord::Base.connection.raw_connection
-        connection.prepare('getTravellers', 'SELECT * FROM travellers LEFT OUTER JOIN users ON travellers.user_email = users.email WHERE trip_id = $1')
+        begin
+            connection.prepare('getTravellers', 'SELECT * FROM travellers LEFT OUTER JOIN users ON travellers.user_email = users.email WHERE trip_id = $1')
+        rescue
+        end
         result = connection.exec_prepared('getTravellers', [params[:trip_id]])
         render json: result
         # render json: User.where(email: Traveller.where(trip_id: params[:trip_id]).select('user_email'))
